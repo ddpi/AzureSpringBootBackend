b411102f6170ae857230a18f02a2752ad1b05667
ddpi <katsuya.oba@gmail.com>
2024-12-08 11:47:46 +0900

    .gitignore修正

--- my-backend-app/src/main/java/com/example/my_backend_app/MyBackendAppApplication.java
+++ my-backend-app/src/main/java/com/example/my_backend_app/MyBackendAppApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class MyBackendAppApplication {
@@ -11,6 +15,18 @@ public class MyBackendAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyBackendAppApplication.class, args);
     }
+	// @Bean
+    // public WebMvcConfigurer corsConfigurer() {
+    //     return new WebMvcConfigurer() {
+    //         @Override
+    //         public void addCorsMappings(CorsRegistry registry) {
+    //             registry.addMapping("/**")
+    //                     .allowedOrigins("https://my-express-frontend-app-c2ccbyf0c4dzhcda.westus2-01.azurewebsites.net") // 許可するオリジンを指定
+    //                     .allowedMethods("GET", "POST", "PUT", "DELETE") // 許可するHTTPメソッドを指定
+    //                     .allowedHeaders("*"); // 許可するヘッダーを指定
+    //         }
+    //     };
+    // }
 }
 
 @RestController
@@ -23,6 +39,6 @@ class MyController {
 
     @GetMapping("/api/hello")
     public String hello() {
-        return "Hello";
+        return "Hello Azure App Service!";
     }
 }
